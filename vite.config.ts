1e90518f7d7cc2b7869e104e5769f593e137af76
HashemRadaideh <hashemalradaideh@gmail.com>
2022-11-22 20:48:19 +0300

    Initial commit.

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import electron from 'vite-electron-plugin'
+import renderer from 'vite-plugin-electron-renderer'
+import pkg from './package.json'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+      'styles': path.join(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    react(),
+    electron({
+      include: ['electron', 'preload',],
+    }),
+    // Use Node.js API in the Renderer-process
+    renderer({
+      nodeIntegration: true,
+    }),
+  ],
+  server: process.env.VSCODE_DEBUG ? (() => {
+    const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+    return {
+      host: url.hostname,
+      port: +url.port,
+    }
+  })() : undefined,
+  clearScreen: false,
+})
