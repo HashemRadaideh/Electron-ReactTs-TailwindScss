f0006282894429a12d5d426b50e7fd1e4fa8f875
HashemRadaideh <hashemalradaideh@gmail.com>
2022-11-26 18:40:48 +0300

    Prettier format.

--- electron/main.ts
+++ electron/main.ts
@@ -1,98 +1,103 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { release } from 'os'
-import { join } from 'path'
+import { app, BrowserWindow, shell, ipcMain } from "electron";
+import { release } from "os";
+import { join } from "path";
 
-process.env.DIST = join(__dirname, '../../dist')
-process.env.PUBLIC = app.isPackaged ? process.env.DIST : join(__dirname, '../../public')
+process.env.DIST = join(__dirname, "../../dist");
+process.env.PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : join(__dirname, "../../public");
 
-const preload = join(__dirname, 'preload.js')
-const url = process.env.VITE_DEV_SERVER_URL
-const indexHtml = join(process.env.DIST, 'index.html')
+const preload = join(__dirname, "preload.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, "index.html");
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === "win32") app.setAppUserModelId(app.getName());
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+  app.quit();
+  process.exit(0);
 }
 
 let mainWindow: BrowserWindow | null = null;
 
 const createWindow = async () => {
   mainWindow = new BrowserWindow({
-    title: 'Main window',
-    icon: join(process.env.PUBLIC, ''),
+    title: "Main window",
+    icon: join(process.env.PUBLIC, ""),
     autoHideMenuBar: true,
     webPreferences: {
       preload: preload,
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
+  });
 
   if (url) {
-    mainWindow.loadURL(url)
+    mainWindow.loadURL(url);
     // Open devTool if the app is not packaged
-    mainWindow.webContents.openDevTools({ mode: 'detach' })
+    mainWindow.webContents.openDevTools({ mode: "detach" });
   } else {
-    mainWindow.loadFile(indexHtml)
+    mainWindow.loadFile(indexHtml);
   }
 
   // Test actively push message to the Electron-Renderer
-  mainWindow.webContents.on('did-finish-load', () => {
-    mainWindow?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
+  mainWindow.webContents.on("did-finish-load", () => {
+    mainWindow?.webContents.send(
+      "main-process-message",
+      new Date().toLocaleString()
+    );
+  });
 
   // Make all links open with the browser, not with the application
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
-}
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
+};
 
 app.on("ready", () => {
-  createWindow()
-})
+  createWindow();
+});
 
-app.on('window-all-closed', () => {
-  mainWindow = null
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  mainWindow = null;
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('second-instance', () => {
+app.on("second-instance", () => {
   if (mainWindow) {
     // Focus on the main window if the user tried to open another
-    if (mainWindow.isMinimized()) mainWindow.restore()
-    mainWindow.focus()
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
   }
-})
+});
 
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
   if (allWindows.length) {
-    allWindows[0].focus()
+    allWindows[0].focus();
   } else {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // new window example arg: new windows url
-ipcMain.handle('open-win', (event, arg) => {
+ipcMain.handle("open-win", (event, arg) => {
   const childWindow = new BrowserWindow({
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
+  });
 
   if (url) {
-    childWindow.loadURL(`${url}#${arg}`)
+    childWindow.loadURL(`${url}#${arg}`);
   } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
+    childWindow.loadFile(indexHtml, { hash: arg });
   }
-})
+});
